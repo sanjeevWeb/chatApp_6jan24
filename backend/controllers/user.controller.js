f40b8f24a6a727e16f4d769359774ddc2b0ccf90
sanjeevWeb <kumarsanjeevdutta02@gmail.com>
2024-01-07 14:15:09 +0530

    added bcryptjs library to encrypt password

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,14 @@
 const user = require("../models/user.model.js")
+const bcrypt = require('bcryptjs')
 
 const getSignedUp = async (req,res) => {
     const {username, email, phone_no, password} = req.body
     if(!username || !email || !phone_no || !password){
         return res.json({ error: 'all fields are required'})
     }
-    const isCreated = await user.create({ username, email, phone_no, password});
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const isCreated = await user.create({ username, email, phone_no, password:hash});
     if(!isCreated){
         return res.json({ error: 'something broke, please retry'})
     }
